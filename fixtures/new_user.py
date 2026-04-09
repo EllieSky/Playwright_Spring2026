7016c0ab17d60a326c7f5fb8a8503e8bd764af86
Ellie Yampolskaya <blackellie@yahoo.com>
2026-04-09 12:05:40 -0400

    Session 19 first pull request ey (#1)
    * Session-19 Includes all playwright tests, utils and configs created so far.
    * Added instructions on how to get started with this repo

---
+++ fixtures/new_user.py
@@ -0,0 +1,25 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+from utils.helpers import generate_user_registration_data
+
+@pytest.fixture
+def get_shopper(page: Page):
+    user = generate_user_registration_data()
+
+    page.goto("/register")
+
+    page.get_by_role("textbox", name="First name:").fill(user.first_name)
+    page.get_by_role("textbox", name="Last name:").fill(user.last_name)
+    ### By text
+    page.get_by_role("textbox", name="Email:").fill(user.email)
+    page.get_by_role("checkbox", name="Newsletter:").uncheck()
+    page.get_by_role("textbox", name="Password:", exact=True).fill(user.password)
+    page.get_by_role("textbox", name="Confirm password:").fill(user.password)
+    page.get_by_role("button", name="Register").click()
+
+    expect(page).not_to_have_url('/registerresult')
+
+    return user.email, user.password
+
+
---
+++ tests/test_search_store.py
@@ -0,0 +1,22 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_basic_store_search(page: Page):
+    search_keyword = "macbook"
+
+    page.goto("https://nop-qa.portnov.com/")
+
+    page.get_by_label("Search store").fill(search_keyword)
+    page.get_by_role("button", name="Search").click()
+
+    expect(page.url).to_contain_text(f"search?q={search_keyword}")
+
+    expect(page.get_by_role("heading", level=1)).to_have_text("Search")
+    (expect(page.get_by_role("heading", level=3)
+            .get_by_role("link"))
+     .to_contain_text(re.compile(search_keyword, re.IGNORECASE)))
+    expect(page.get_by_role("heading", level=2).locator("a"))
+    expect(page.get_by_role("link")).to_be_visible()
+
+
