a3a9e872073db7cca7268fde65557a35634e1816
Ellie Yampolskaya <ellie@lula.is>
2026-04-10 19:36:40 -0400

    Session 20: bug fixes

--- tests/test_search_store.py
+++ tests/test_search_store.py
@@ -10,13 +10,13 @@ def test_basic_store_search(page: Page):
     page.get_by_label("Search store").fill(search_keyword)
     page.get_by_role("button", name="Search").click()
 
-    expect(page.url).to_contain_text(f"search?q={search_keyword}")
+    assert f"search?q={search_keyword}" in page.url
+    # OR
+    expect(page).to_have_url(f"search?q={search_keyword}")
 
     expect(page.get_by_role("heading", level=1)).to_have_text("Search")
-    (expect(page.get_by_role("heading", level=3)
-            .get_by_role("link"))
+    (expect(page.get_by_role("heading", level=2).get_by_role("link"))
      .to_contain_text(re.compile(search_keyword, re.IGNORECASE)))
-    expect(page.get_by_role("heading", level=2).locator("a"))
-    expect(page.get_by_role("link")).to_be_visible()
+    expect(page.get_by_role("heading", level=2).locator("a")).to_be_visible()
 
 
