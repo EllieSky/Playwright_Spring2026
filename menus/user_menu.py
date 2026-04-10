fa743f3b35d4f6386514b1e4cd16b586c2cc710a
Ellie Yampolskaya <blackellie@yahoo.com>
2026-04-10 19:38:13 -0400

    Session 20: bug fixes

--- menus/user_menu.py
+++ menus/user_menu.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 class UserMenu:
 
     def __init__(self, page: Page):
-        super().__init__(page)
+        # super().__init__(page)
         self.page = page
         self.link_register = page.get_by_role("link", name="Register")
         self.link_login = page.get_by_role("link", name="Log in")
@@ -11,7 +11,8 @@ class UserMenu:
         self.link_shipping_cart = page.get_by_role("link", name="Shopping cart (0)")
         self.fld_search_store = page.get_by_role("textbox", name="Search store")
         self.btn_search = page.get_by_role("button", name="Search")
-        self.link_my_account = page.get_by_role("link", name="My account").first
+        self.link_my_account = page.locator('.header-links .ico-account')  # Unique locator
+        # self.link_my_account = page.get_by_role("link", name="My account").first
         self.link_logout = page.get_by_role("link", name="Log out")
 
     def goto_login(self):
